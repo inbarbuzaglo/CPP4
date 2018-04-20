d7b4ce985cb841738caced3e34d2c27dac8271d2
InbarBuzaglo <33480360+inbarbuzaglo@users.noreply.github.com>
2018-04-20 13:47:23 +0300

    Update CircularInt.h

--- CircularInt.h
+++ CircularInt.h
@@ -1,16 +1,35 @@
-
 #include <iostream>
+#include "CNode.h"
 
 using namespace std;
 
 class CircularInt
 {
+    private:
     int start;
     int end;
-    Node *head;
 
     public:
+    CNode* head; 
+    CNode* current;
 
-    CircularInt(int start_m,int end_m);
     void insert(int x);
+    CircularInt(int start_input,int end_input);
+    CircularInt& operator++();
+    CircularInt& operator++(int);
+    CircularInt& operator--(int);
+    CircularInt& operator+=(int value); 
+    CircularInt& operator-=(int value);
+    CircularInt& operator*=(int value); 
+     CircularInt& operator/=(int value);
+    const CircularInt& operator= (int value);
+    bool operator==(const CircularInt other)const;
+    bool operator!=(const CircularInt other)const;
+    int& operator*();
+    operator bool() ;
+        
+        ~CircularInt();
+
 };
+ 
+ ostream &operator<<( ostream &output, const CircularInt& ci);
