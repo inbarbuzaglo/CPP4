754105e7df4cb1476f1c513e0b26b8745251b2cb
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2018-04-21 21:47:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
-
 #include <iostream>
+#include "CircularInt.h"
+
 using namespace std;
-
-#include "CircularInt.hpp"
-
 
 int main() {
 	CircularInt hour {1, 12};                 // <hour is an integer between 1 and 12, like an hour on the clock>
 	cout << hour << endl;                     // 1
-	hour += 4;  cout << hour << endl;         // 5
-	(hour += 2)++;  cout << hour << endl;     // 8
-	hour += 18;   cout << hour << endl;       // 2   (18 hours after 8)
+	hour += 4; 
+	cout << hour << endl;         // 5
+	(hour += 2)++;  
+	cout << hour << endl;     // 8
+	hour += 18;  
+    cout << hour << endl;       // 2   (18 hours after 8)
 	cout << -hour << endl;                    // 10  (2 hours before midnight)
-	hour = 1 - hour; cout << hour << endl;    // 11  (2 hours before 1)
+	hour = 1- hour;
+    cout << hour << endl;    // 11  (2 hours before 1)
 	cout << hour+hour << endl;                // 10 (11 hours after 11)
-	hour *= 2;   cout << hour << endl;        // 10 (11*2 = 11+11)
+	hour *= 2;   
+	cout << hour << endl;        // 10 (11*2 = 11+11)
 	cout << hour/2 << endl;                   // TWO OPTIONS: 11 (since 11*2=10) or 5 (since 5*2=10 too).
 
 	try {
