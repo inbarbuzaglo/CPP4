1fe9746cb7bd9844c68fb6fcd66144bb0fc4e218
InbarBuzaglo <33480360+inbarbuzaglo@users.noreply.github.com>
2018-04-26 18:36:55 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -14,22 +14,21 @@ class CircularInt
 
     void insert(int x);
     CircularInt(int start_input,int end_input);
-    CircularInt& operator++();
-    CircularInt& operator++(int value);
-    CircularInt& operator+(int value);
-    CircularInt& operator+=(int value); 
-    CircularInt& operator*=(int value); 
-    CircularInt& operator/(int value);
-    operator bool();
-    CircularInt& operator-(); 
-    CircularInt& operator--(int value);
+    CircularInt& operator++(int);//check
+    CircularInt& operator+();//check
+    CircularInt& operator+=(int value); //check
+    CircularInt& operator*=(int value); //check
+    CircularInt& operator/(int value);//needs to be completed+add /=
+    operator bool();//check
+    CircularInt& operator-();//check //needs to be added -(const circularint other)
+    CircularInt& operator--(int);//check
     CircularInt& operator-=(int value);
-   
-    CircularInt& operator*(const CircularInt other);
-    CircularInt& operator=(const CircularInt other);
-    bool operator==(const CircularInt other)const;
-    bool operator!=(const CircularInt other)const;
-    CircularInt& operator+(const CircularInt other);
+    CircularInt& operator/(const CircularInt other);//needs to be completed
+    CircularInt& operator*(const CircularInt other);//check
+    //add operator=(int value)
+    bool operator==(const CircularInt other)const;//check
+    bool operator!=(const CircularInt other)const;//check
+    CircularInt& operator+(const CircularInt other);//check
   
     
         
