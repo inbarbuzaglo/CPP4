6064fc412f5aa394854002f42ec8dd9aa81808a8
InbarBuzaglo <33480360+inbarbuzaglo@users.noreply.github.com>
2018-04-26 16:34:23 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -17,18 +17,19 @@ class CircularInt
     CircularInt& operator++();
     CircularInt& operator++(int value);
     CircularInt& operator+(int value);
-    CircularInt& operator+(const CircularInt other);
     CircularInt& operator+=(int value); 
     CircularInt& operator*=(int value); 
-     CircularInt& operator/(int value);
-    bool operator==(const CircularInt other)const;
-    bool operator!=(const CircularInt other)const;
-    const CircularInt& operator=(int value);
-     operator bool();
+    CircularInt& operator/(int value);
+    operator bool();
     CircularInt& operator-(); 
     CircularInt& operator--(int value);
     CircularInt& operator-=(int value);
- 
+   
+    CircularInt& operator*(const CircularInt other);
+    CircularInt& operator=(const CircularInt other);
+    bool operator==(const CircularInt other)const;
+    bool operator!=(const CircularInt other)const;
+    CircularInt& operator+(const CircularInt other);
   
     
         
