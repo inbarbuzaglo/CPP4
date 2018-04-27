75b82b76f81e1f480da4c3b72a00c73d662bdd37
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2018-04-27 09:41:52 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -46,10 +46,10 @@ class CircularInt
 
 bool operator==(int value, const CircularInt other);  
 bool operator!=(int value, const CircularInt other); 
-bool operator<(int value, const CircularInt other);
-bool operator>(int value, const CircularInt other);
-//bool operator<=(int value, const CircularInt other);
-//bool operator>=(int value, const CircularInt other); 
+//bool operator<(int value, const CircularInt other);
+//bool operator>(int value, const CircularInt other);
+bool operator<=(int value, const CircularInt other);
+bool operator>=(int value, const CircularInt other); 
 ostream &operator<<(ostream &output, const CircularInt& ci);
 istream &operator>>(istream &input, const CircularInt& ci);
 
