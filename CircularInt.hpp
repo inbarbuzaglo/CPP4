54fa4946e7cccbead679d730034bfaea1ddd6eae
InbarBuzaglo <33480360+inbarbuzaglo@users.noreply.github.com>
2018-04-28 19:51:57 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -1,61 +1,71 @@
-#include <iostream>
-#include "CNode.hpp"
+#pragma once
 
+#include <iostream>  
 using namespace std;
 
-class CircularInt
-{
-    private:
-    int start;
-    int end;
+class CircularInt {
+private:
+	int min1;
+	int max1;
+	int currentval;
+	int range;
+	int CurrentValue(int value);
 
-    public:
-    CNode* current;
+public:
+	CircularInt(int min1, int high);
+	CircularInt();
+	CircularInt(const CircularInt& other);
 
-    void insert(int x);
-    CircularInt(int start_input,int end_input);
-    CircularInt& operator++(int);//check
-    CircularInt& operator+();//check
-    CircularInt& operator+(int value);
-    CircularInt& operator+=(int value); //check
-    CircularInt& operator*=(int value);//check
-    CircularInt& operator*=(const CircularInt other); 
-    CircularInt& operator/(int value);//needs to be completed+add /=
-   // CircularInt& operator%(int value);
-    operator bool();//check
-    CircularInt& operator-();//check 
-    CircularInt& operator--(int);//check
-    CircularInt& operator-=(int value);
-    CircularInt& operator-(int value); //number-object   
-    CircularInt& operator/(const CircularInt other);//needs to be completed
-    CircularInt& operator%=(int value);
-    CircularInt& operator%=(const CircularInt other);
-    CircularInt& operator*(const CircularInt other); //check
-    CircularInt& operator=(int value);
-    bool operator==(const CircularInt other)const;//check
-    bool operator==(int value);
-    bool operator!=(const CircularInt other)const;//check
-    bool operator!=(int value);
-    bool operator>(const CircularInt other);
-    bool operator<(const CircularInt other);
-    bool operator>(int value);
-	bool operator<(int value);
-    bool operator>=(const CircularInt other);
-    bool operator<=(const CircularInt other);
-    bool operator>=(int value);
-    bool operator<=(int value);
-    CircularInt& operator+(const CircularInt other);//check
-        
-        ~CircularInt();
+	CircularInt& operator=(int c);
+	CircularInt& operator+=(int const &value);
+	CircularInt& operator+=(CircularInt const &value);
+    CircularInt& operator++();
+	CircularInt operator++(int);
+    CircularInt& operator+(int const &value);
+	CircularInt& operator+(CircularInt const &value);
+	CircularInt& operator-=(int const &value);
+	CircularInt& operator-=(CircularInt const &value);
+	CircularInt& operator--();
+	CircularInt operator--(int);
+	CircularInt& operator-();
+	CircularInt& operator-(int const &value);
+	CircularInt& operator-(CircularInt const &value);
+	CircularInt& operator*(int const &value);
+	CircularInt& operator*(CircularInt const &value);
+    CircularInt& operator*=(int const &value);
+	CircularInt& operator*=(CircularInt const &value);
+	CircularInt& operator/(int const &value);
+	CircularInt& operator/(CircularInt const &value);
+    CircularInt& operator/=(int const &value);
+	CircularInt& operator/=(CircularInt const &value);
+	CircularInt& operator%(int const &value);
+	CircularInt& operator%(CircularInt const &value);
+	CircularInt& operator%=(int const &value);
+	CircularInt& operator%=(CircularInt const &value);
+
+	bool operator ==(CircularInt const& c);
+	bool operator !=(CircularInt const& c);
+	bool operator ==(int const& c);
+	bool operator !=(int const& c);
+	bool operator >(CircularInt const& c);
+	bool operator <(CircularInt const& c);
+	bool operator >(int const& c);
+	bool operator <(int const& c);
+	bool operator >=(CircularInt const& c);
+	bool operator <=(CircularInt const& c);
+	bool operator >=(int const& c);
+	bool operator <=(int const& c);
+
+    friend bool operator==(int val, const CircularInt& c);
+	friend bool operator!=(int val, const CircularInt& c);
+    friend bool operator>(int val, const CircularInt& c);
+	friend bool operator<(int val, const CircularInt& c);
+	friend bool operator>=(int val, const CircularInt& c);
+	friend bool operator<=(int val, const CircularInt& c);
+
+    friend CircularInt operator-(int val, const CircularInt& c);
+	friend CircularInt operator+(int val, const CircularInt& c);
+    friend CircularInt operator/(int val, const CircularInt& c);
+    friend ostream& operator<<(ostream& os, const CircularInt& o);
+	friend istream& operator>>(istream & input, CircularInt &o);
 };
-
-bool operator==(int value, const CircularInt other);  
-bool operator!=(int value, const CircularInt other); 
-//bool operator<(int value, const CircularInt other);
-//bool operator>(int value, const CircularInt other);
-//bool operator<=(int value, const CircularInt other);
-//bool operator>=(int value, const CircularInt other); 
-ostream &operator<<(ostream &output, const CircularInt& ci);
-istream &operator>>(istream &input, const CircularInt& ci);
-
-
