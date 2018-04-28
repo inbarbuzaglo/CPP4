7bb59b40c6dbf7a01fdc66aa660e5f8069dc8381
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2018-04-28 20:47:05 +0300

    Update CircularInt.cpp

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -1,19 +1,21 @@
 #include "CircularInt.hpp"
 #include <algorithm>
 #include <string>
+#include <iostream>
 using namespace std;
 
 int CircularInt::CurrentValue(int value)
 {
-    int cur=((value - min1) % (range) + range) % (range) + min1;
-	return cur;
+    int current=((value - min1) % (range) + range) % (range) + min1;
+	return current;
 }
 CircularInt::CircularInt(int low, int high)
 {
 	max1 = max(high, low);
     min1 = min(low, high);
-	range = abs(max1 - min1) + 1;
     currentval = min1;
+	range = abs(max1 - min1) + 1;
+   
 }
 CircularInt::CircularInt(const CircularInt & other)
 {
@@ -86,7 +88,7 @@ CircularInt CircularInt::operator--(int)
 	return temp;
 }
 
-CircularInt & CircularInt::operator*=(int const & value)
+CircularInt & CircularInt::operator*=(int const & value) 
 {
 	currentval = CurrentValue(currentval * value);
 	return *this;
@@ -134,7 +136,7 @@ CircularInt & CircularInt::operator%=(CircularInt const & value)
 
 bool CircularInt::operator==(CircularInt const & c)
 {
-	return this->min1 == c.min1 && this->max1 == c.max1 && this->currentval == c.currentval;
+	return (this->max1 == c.max1) && (this->min1 == c.min1) && (this->currentval == c.currentval);
 }
 
 bool CircularInt::operator!=(CircularInt const & c)
@@ -144,12 +146,12 @@ bool CircularInt::operator!=(CircularInt const & c)
 
 bool CircularInt::operator==(int const & c)
 {
-	return this->currentval == c;
+	return (this->currentval) == c;
 }
 
 bool CircularInt::operator!=(int const & c)
 {
-	return this->currentval != c;
+	return (this->currentval) != c;
 }
 
 bool CircularInt::operator>(CircularInt const & c)
@@ -306,7 +308,7 @@ CircularInt & CircularInt::operator/(int const & value)
 	CircularInt* temp = new CircularInt(*this);
 	if (currentval % value != 0)
 	{
-		throw string(string("There is no numcer x in { ") + to_string(min1) + string(",") + to_string(max1) + " } such that x * " + to_string(value) + " = " + to_string(currentval));
+		throw string(string("There is no number x in { ") + to_string(min1) + string(",") + to_string(max1) + " } such that x * " + to_string(value) + " = " + to_string(currentval));
 	}
 	int res = currentval / value;
 	temp->currentval = CurrentValue(res);
@@ -317,7 +319,7 @@ CircularInt & CircularInt::operator/(CircularInt const & value)
 {
 	if (currentval % value.currentval != 0)
 	{
-		throw string(string("There is no numcer x in { ") + to_string(min1) + string(",") + to_string(max1) + " } such that x * " + to_string(value.currentval) + " = " + to_string(currentval));
+		throw string(string("There is no number x in { ") + to_string(min1) + string(",") + to_string(max1) + " } such that x * " + to_string(value.currentval) + " = " + to_string(currentval));
 	}
 	CircularInt* temp = new CircularInt(*this);
 	int res = currentval / value.currentval;
@@ -338,10 +340,10 @@ CircularInt & CircularInt::operator%(CircularInt const & value)
 	temp->currentval = CurrentValue(res);
 	return *temp;
 }
-ostream& operator<<(ostream& os, const CircularInt& o)
+ostream& operator<<(ostream& output, const CircularInt& o)
 {
-	os << o.currentval;
-	return os;
+	output << o.currentval;
+	return output;
 }
 istream& operator>>(istream & input, CircularInt &o)
 {
